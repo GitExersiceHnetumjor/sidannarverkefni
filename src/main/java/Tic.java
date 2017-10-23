38bb73db0c88872bb4fdf572c769aea975d74308
Hannes <hannes16@ru.is>
2017-10-23 16:18:28 +0000

    updateBoard funciton added

--- src/main/java/Tic.java
+++ src/main/java/Tic.java
@@ -1,8 +1,12 @@
 
 public class Tic {
 	private static char board[];
+	private static int turns;
+	private static char activePlayer;
 	
 	public Tic() {
+		turns = 0;
+		activePlayer = 'X';
 		board = new char[9];
 
 		for (int i = 0; i < 9; i++) {
@@ -10,6 +14,26 @@ public class Tic {
 			char b = (char)(k + '0');
 			board[i] = b;
 		}
+	}
+
+	public static boolean updateBoard(int t) {
+	
+		if (board[t - 1] == 'X' || board[t - 1] == 'O' ) {
+			return false;
+		}
+		turns++;
+		if (activePlayer == 'X') {
+			board[t - 1] = 'X';
+			
+			activePlayer = 'O';
+			
+		} else {
+			board[t - 1] = 'O';
+			
+			activePlayer = 'X';
+			
+		}
+		return true;
 	}
 }
 
--- src/test/java/TicTest.java
+++ src/test/java/TicTest.java
@@ -5,6 +5,16 @@ import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 public class TicTest {
+	@Test
+	public void testInvalidUpdateBoard() {
+		Tic.updateBoard(5);
+		assertFalse(Tic.updateBoard(5));		
+	}
 
+	@Test
+	public void testValidUpdateBoard() {
+		Tic t = new Tic();
+		assertTrue(t.updateBoard(8));		
+	}
 	
 }
