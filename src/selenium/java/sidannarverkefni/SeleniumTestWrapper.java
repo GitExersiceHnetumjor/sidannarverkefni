091a87a20017eec7293808e3f5481f7d42db487e
Hannes <hannes16@ru.is>
2017-10-24 14:29:12 +0000

    selenum problem fixing

---
+++ src/selenium/java/sidannarverkefni/SeleniumTestWrapper.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.*;
+import java.util.concurrent.TimeUnit;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public abstract class SeleniumTestWrapper {
+	static WebDriver driver;
+	static String baseUrl = "https://sidannar-tictactoe.herokuapp.com/";
+	
+
+	@BeforeClass
+	public static void openBrowser() {
+		driver = new FirefoxDriver();
+		driver.get(baseUrl);
+
+	}
+
+	@AfterClass
+	public static void closeBrowser() {
+		driver.quit();
+	}
+}
