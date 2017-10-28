3746b02b983a9918c522c50bb0cb7693b2d0246b
Ivar Kristjansson <ivark16@ru.is>
2017-10-28 15:50:09 +0000

    Changing filename in selenium

---
+++ src/selenium/java/SeleniumTestWrapper.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public abstract class SeleniumTestWrapper {
+  static ChromeDriver driver;
+  static String baseUrl;
+  static String port;
+
+  @BeforeClass
+  public static void openBrowser() {
+    driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    //port = System.getenv("PORT");
+    //if (port == null) {
+      //  port = "4567";
+    //}
+    baseUrl = "https://sidannar-tictactoe.herokuapp.com/";
+  }
+
+  @AfterClass
+  public static void closeBrowser(){
+    driver.quit();
+  }
+}
