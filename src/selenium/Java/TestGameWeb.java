7708caf0130f089e2ad0d126ce664e62c21eadbe
Ivar Kristjansson <ivark16@ru.is>
2017-10-28 14:56:10 +0000

    Added buildscripts to travis file, updated build.gradle for xvfb and added import for selenium tests

--- src/selenium/Java/TestGameWeb.java
+++ src/selenium/Java/TestGameWeb.java
@@ -1,9 +1,11 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver
 
 public class TestGameWeb extends SeleniumTestWrapper {
 
